3cc32452b7944ed81265d91993f977c926b69077
David W <davito441@gmail.com>
2022-06-18 22:27:08 -0500

    Updating Files

---
+++ Project Kawaii/Assets/Scripts/Menus/TextMover.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class TextMover : MonoBehaviour
+{
+    public int offsetX = 0, offsetY = 10;
+    private RectTransform textRect;
+    private Vector3 pos;
+
+    void Start()
+    {
+        textRect = gameObject.GetComponent<RectTransform>();
+        pos = textRect.localPosition;
+    }
+
+    public void Down()
+    {
+        textRect.localPosition = new Vector3(pos.x + offsetX, pos.y - offsetY, pos.z);
+    }
+
+    public void Up()
+    {
+        textRect.localPosition = pos;
+    }
+}
